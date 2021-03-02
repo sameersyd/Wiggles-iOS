9b433acd5469d11fa368a0d25e067e4317787960
Sameer Nawaz <sameer.nwaz@gmail.com>
2021-03-02 11:24:34 +0530

    Initial Commit

---
+++ Wiggles-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wiggles-iOS
+//
+//  Created by Sameer Nawaz on 02/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wiggles-iOS/Wiggles_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wiggles_iOSApp.swift
+//  Wiggles-iOS
+//
+//  Created by Sameer Nawaz on 02/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Wiggles_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
